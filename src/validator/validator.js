224537c09735325cf2ba19ce2b6f38c29437e5a2
konrad odo <konrad.odo@erli.pl>
2022-01-27 09:43:14 +0100

    Fix validation in date picker

--- src/validator/validator.js
+++ src/validator/validator.js
@@ -4,7 +4,7 @@ export const validator = {
   isRequire: (value) => (!(value !== null && value !== undefined && value.length) ? 'Field is required' : null),
   isStringMax100: (value) => validator.isRequire(value) || (value.length >= 100 ? 'Title is too long (max 100)' : null),
   isDate: (value) => (!(value && moment(value, 'YYYY-M-D', true).isValid()) ? 'Date is invalid' : null),
-  isTodayOrAfter: (value) => validator.isDate(value) || (moment(value).isBefore(moment()) ? 'Date cannot be from the past' : null),
+  isTodayOrAfter: (value) => validator.isDate(value) || (moment(value).isBefore(moment().subtract(1, 'day')) ? 'Date cannot be from the past' : null),
 };
 
 export const validatorByName = (name) => validator[name] || (() => false);
